62a8e9291ac487eddd70155109e03514ae4deb4f
mulualeme <bdu1404544@gmail.com>
2024-12-27 16:25:08 +0300

    minor

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,14 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://gebs-bakery.vercel.app/"
+        ? [
+            "https://gebs-bakery.vercel.app",
+            "https://gebs-bakery-api.vercel.app",
+          ]
         : ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
